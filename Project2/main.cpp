633e095b8a97e32ba506757557c319dbc984d826
Dasadno <Yakaleks96@gmail.com>
2024-10-02 15:20:10 +0500

    Добавьте файлы проекта.

---
+++ Project2/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	const int arrlenght = 20;
+	int arr[arrlenght];
+
+	for (int i = 0; i < 20; i++) {
+
+		arr[i] = rand() % 16 - 5;
+		cout << arr[i] << ' ';
+		cout << endl;
+	}
+
+	return 0;
+}
