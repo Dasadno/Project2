1c3f6a278c3efc235873f80a76c29980b5982f8c
Dasadno <Yakaleks96@gmail.com>
2024-10-02 16:16:21 +0500

    sadads

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -6,17 +6,29 @@ int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	const int arrlenght = 20;
-	int arr[arrlenght];
+	const int arrlenght = 10;
+	int rand_arr[arrlenght];
+	int user_arr[arrlenght];
+	int answer;
+	int rand_num;
 
-	for (int i = 0; i < 20; i++) {
-
-		arr[i] = rand() % 16 - 5;
-		cout << arr[i] << ' ';
-		if (i == 9)
+	for (int i = 0; i < 10; i++) {
+		rand_num = rand() % 16 - 5;
+		rand_arr[i] = rand_num;
+		cout << "Введите число от 0 до 10: ";
+		cin >> answer;
+		user_arr[i] = answer;
+		if (answer < 0 || answer > 10)
 		{
-			cout << endl;
+			cout << "Вы указали число неправильно! \n";
+			i--;
+			continue;
 		}
+		if (answer == rand_num)
+		{
+			cout << "Рандомайзер ввел такое же число! :) \n";
+		}
+
 	}
 
 	return 0;
